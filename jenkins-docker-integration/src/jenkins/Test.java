65c5ff5792f871e0cca43644e501d94293557a3a
cv-sreedhar <sridharchennur@gmail.com>
2022-04-15 16:21:29 +0530

    first commit...

---
+++ jenkins-docker-integration/src/jenkins/Test.java
@@ -0,0 +1,12 @@
+package jenkins;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("First Project for CICD");
+
+	}
+
+}
